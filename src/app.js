9843a69ec378ac54771723c629081c8ae91334a8
김성민 <kimsungmin@gimseongmin-ui-MacBookAir.local>
2024-02-23 15:38:32 +0900

    3차

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import LogMiddleware from "./src/middlewares/log.middleware.js";
 import ErrorHandlingMiddleware from "./src/middlewares/error-handling.middleware.js";
-import router from "./index.js";
+import router from "./router.js";
 
 const app = express();
 const PORT = 3020;
