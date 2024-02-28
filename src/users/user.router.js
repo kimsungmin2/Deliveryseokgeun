06f4c67c8a9da558af6c11b40179031eca0b0474
choobao <154485888+choobao@users.noreply.github.com>
2024-02-28 12:02:45 +0900

    fix: merge 후 에러 fix & 테스트 완료, 일부 수정 필요

--- src/users/user.router.js
+++ src/users/user.router.js
@@ -10,15 +10,17 @@ import { PointsService } from "../points/point.service.js";
 import { adauthMiddleware } from "../middlewares/adauth.middlewares.js";
 import { OrdersRepository } from "../orders/order.repository.js";
 
-
 const router = express.Router();
 const usersRepository = new UsersRepository(prisma, redisClient);
 const ordersRepository = new OrdersRepository(prisma);
 const pointsRepository = new PointsRepository(prisma);
-const usersService = new UsersService(usersRepository, pointsRepository, ordersRepository);
+const usersService = new UsersService(
+  usersRepository,
+  pointsRepository,
+  ordersRepository
+);
 const pointsService = new PointsService(pointsRepository);
 const usersController = new UsersController(usersService, pointsService);
-
 
 router.post("/signin", usersController.signIn);
 router.post("/adsignin", usersController.adsignIn);
@@ -28,7 +30,11 @@ router.post("/adusers", usersController.adminregister);
 
 router.get("/point", authMiddleware, usersController.getUserPoint);
 router.patch("/users/:userId", authMiddleware, usersController.userEdit); //
-router.patch("/adusers/:aduserId", adauthMiddleware, usersController.aduserEdit); //
+router.patch(
+  "/adusers/:aduserId",
+  adauthMiddleware,
+  usersController.aduserEdit
+); //
 router.get("/users/:userId", usersController.getUser);
 router.get("/users", usersController.getUsermany);
 router.get("/adusers/:aduserId", usersController.getadUser);
@@ -36,11 +42,19 @@ router.get("/adusers", usersController.getadUsermany);
 router.patch("/useraceess", usersController.useraceess);
 router.patch("/aduseraceess", usersController.aduseraceess);
 
-router.delete("/users/:userId", authMiddleware, usersController.userdelete); //
-router.delete("/adusers/:aduserId", adauthMiddleware, usersController.aduserdelete); //
+router.delete("/users/:userId", authMiddleware, usersController.userdelete);
+router.delete(
+  "/adusers/:aduserId",
+  adauthMiddleware,
+  usersController.aduserdelete
+);
 
 router.patch("/:userId", authMiddleware, usersController.userEdit);
-router.patch("/adusers/:aduserId", adauthMiddleware, usersController.aduserEdit);
+router.patch(
+  "/adusers/:aduserId",
+  adauthMiddleware,
+  usersController.aduserEdit
+);
 
 router.get("/:userId", usersController.getUser);
 router.get("/", usersController.getUsermany);
@@ -48,15 +62,14 @@ router.get("/", usersController.getUsermany);
 router.get("/:aduserId", usersController.getadUser);
 router.get("/adusers", usersController.getadUsermany);
 
-
 router.patch("/useraceess", usersController.useraceess);
 router.patch("/aduseraceess", usersController.aduseraceess);
 
-
 router.delete("/:userId", authMiddleware, usersController.userdelete);
-router.delete("/adusers/:aduserId", adauthMiddleware, usersController.aduserdelete);
-
-
-
+router.delete(
+  "/adusers/:aduserId",
+  adauthMiddleware,
+  usersController.aduserdelete
+);
 
 export default router;
--- src/users/user.service.js
+++ src/users/user.service.js
@@ -4,64 +4,74 @@ import bcrypt from "bcrypt";
 import { sendVerificationEmail } from "../middlewares/sendEmail.middlewares.js";
 dotenv.config();
 export class UsersService {
-    constructor(usersRepository, pointsRepository, ordersRepository) {
-        this.usersRepository = usersRepository;
-        this.pointsRepository = pointsRepository;
-        this.ordersRepository = ordersRepository;
-    }
-  
-   signIn = async (email) => {
-        const user = await this.usersRepository.getUserByEmail(email);
-        const rating = await this.ordersRepository.ratingUserPoint(user.userId);
-        const userpoint = rating[0]._sum.totalPrice;
-     
-     
-    if(user === null){
-      throw new Error("유저가 존재하지 않습니다.")
+  constructor(usersRepository, pointsRepository, ordersRepository) {
+    this.usersRepository = usersRepository;
+    this.pointsRepository = pointsRepository;
+    this.ordersRepository = ordersRepository;
+  }
+
+  signIn = async (email) => {
+    const user = await this.usersRepository.getUserByEmail(email);
+    // const rating = await this.ordersRepository.ratingUserPoint(user.userId);
+    // const userpoint = rating[0]._sum.totalPrice;
+
+    if (user === null) {
+      throw new Error("유저가 존재하지 않습니다.");
     }
 
-    if(user.emailStatus !== "completion"){
+    if (user.emailStatus !== "completion") {
       throw new Error("활성화 되지 않은 어드민입니다.");
     }
 
-        if (userpoint > 1000000) {
-            await this.usersRepository.ratingepicUpdate(user.userId);
-        } else if (userpoint > 500000) {
-            await this.usersRepository.ratingrareUpdate(user.userId);
-        }
+    // if (userpoint > 1000000) {
+    //   await this.usersRepository.ratingepicUpdate(user.userId);
+    // } else if (userpoint > 500000) {
+    //   await this.usersRepository.ratingrareUpdate(user.userId);
+    // }
 
-        const userJWT = jwt.sign({ userId: user.userId }, process.env.JWT_SECRET, {
-            expiresIn: "12h",
-        });
+    const userJWT = jwt.sign({ userId: user.userId }, process.env.JWT_SECRET, {
+      expiresIn: "12h",
+    });
 
-        const refreshToken = jwt.sign({ userId: user.userId }, process.env.REFRESH_SECRET, { expiresIn: "7d" });
-        console.log(userJWT);
-        return { userJWT, refreshToken };
-    };
- 
+    const refreshToken = jwt.sign(
+      { userId: user.userId },
+      process.env.REFRESH_SECRET,
+      { expiresIn: "7d" }
+    );
+    console.log(userJWT);
+    return { userJWT, refreshToken };
+  };
 
   adsignIn = async (adEmail) => {
     const aduser = await this.usersRepository.getAdByEmails(adEmail);
 
-    if(aduser === null){
-      throw new Error("유저가 존재하지 않습니다.")
+    if (aduser === null) {
+      throw new Error("유저가 존재하지 않습니다.");
     }
-    
-    if(aduser.adEmailStatus !== "completion"){
+
+    if (aduser.adEmailStatus !== "completion") {
       throw new Error("활성화 되지 않은 어드민입니다.");
     }
 
-    const userJWT = jwt.sign({ aduserId: aduser.aduserId }, process.env.JWT_SECRET, { expiresIn: "12h" });
-    const refreshToken = jwt.sign({ aduserId: aduser.aduserId }, process.env.REFRESH_SECRET, { expiresIn: "7d" });
+    const userJWT = jwt.sign(
+      { aduserId: aduser.aduserId },
+      process.env.JWT_SECRET,
+      { expiresIn: "12h" }
+    );
+    const refreshToken = jwt.sign(
+      { aduserId: aduser.aduserId },
+      process.env.REFRESH_SECRET,
+      { expiresIn: "7d" }
+    );
     return { userJWT, refreshToken };
-};
+  };
 
   hashPassword = async (password) => {
     const saltRounds = 10;
     const hashedPassword = await bcrypt.hash(password, saltRounds);
     return hashedPassword;
   };
-   /// 고객님 회원가입
+  /// 고객님 회원가입
   register = async (email, name, password) => {
     const user = await this.usersRepository.getUserByEmail(email);
     if (user) {
@@ -69,8 +79,8 @@ export class UsersService {
     }
     const hashedPassword = await this.hashPassword(password);
     const randomNum = () => {
-        return Math.floor(1000 + Math.random() * 9000);
-      };
+      return Math.floor(1000 + Math.random() * 9000);
+    };
     const token = randomNum();
     const usercreate = await this.usersRepository.registercreate(
       email,
@@ -108,14 +118,13 @@ export class UsersService {
   };
 
   getUser = async (userId) => {
-
     const users = await this.usersRepository.getUserById(userId);
 
-    if(users === null){
+    if (users === null) {
       throw new Error("조회하시는 유저가 존재하지 않습니다.");
     }
 
-    if(users.emailStatus !== "completion"){
+    if (users.emailStatus !== "completion") {
       throw new Error("활성화 되지 않은 유저입니다.");
     }
 
@@ -129,14 +138,13 @@ export class UsersService {
   };
 
   getadUser = async (aduserId) => {
-
     const adusers = await this.usersRepository.getadUserById(aduserId);
 
-    if(adusers === null){
+    if (adusers === null) {
       throw new Error("조회하시는 어드민이 존재하지 않습니다.");
     }
 
-    if(adusers.adEmailStatus !== "completion"){
+    if (adusers.adEmailStatus !== "completion") {
       throw new Error("활성화 되지 않은 유저입니다.");
     }
 
@@ -151,109 +159,99 @@ export class UsersService {
 
   userEdit = async (userId, email, password, name) => {
     const userIds = await this.usersRepository.userById(userId);
-    const hashedPassword = this.hashPassword(password);
-    
-    if(!userIds){
+
+    if (!userIds) {
       throw new Error("유저가 존재하지 않습니다.");
     }
 
-    if(userId !== userIds.userId){
+    if (userId !== userIds.userId) {
       throw new Error("어드민 아이디가 일치하지 않습니다");
     }
 
-    if(userIds.emailStatus !== "completion"){
+    if (userIds.emailStatus !== "completion") {
       throw new Error("활성화 되지 않은 유저입니다.");
     }
 
     const passwordMatch = await bcrypt.compare(password, userIds.password);
 
     if (!passwordMatch) {
-        throw new Error("현재 비밀번호가 일치하지 않습니다.");
+      throw new Error("현재 비밀번호가 일치하지 않습니다.");
     }
 
     const update = await this.usersRepository.userEdit(userId, name);
     return update;
-  }
+  };
 
   aduserEdit = async (aduserId, adEmail, adPassword, adminName) => {
     const aduser = await this.usersRepository.getAdByEmails(adEmail);
-    const hashedPassword = this.hashPassword(adPassword);
 
-    if(!aduser){
+    if (!aduser) {
       throw new Error("어드민이 존재하지 않습니다");
     }
 
-    if(aduserId !== aduser.aduserId){
+    if (aduserId !== aduser.aduserId) {
       throw new Error("어드민 아이디가 일치하지 않습니다");
     }
 
-    if(aduser.adEmailStatus !== "completion"){
+    if (aduser.adEmailStatus !== "completion") {
       throw new Error("활성화 되지 않은 어드민입니다.");
     }
 
     const passwordMatch = await bcrypt.compare(adPassword, aduser.adPassword);
 
     if (!passwordMatch) {
-        throw new Error("현재 비밀번호가 일치하지 않습니다.");
+      throw new Error("현재 비밀번호가 일치하지 않습니다.");
     }
 
-    const update = await this.usersRepository.aduserEdit(aduserId, hashedPassword, adminName);
+    const update = await this.usersRepository.aduserEdit(aduserId, adminName);
 
     return update;
-  }
+  };
 
   userdelete = async (userId, password) => {
     const user = await this.usersRepository.userById(userId);
-  
-    if(!user) {
+
+    if (!user) {
       throw new Error("유저가 존재하지 않습니다");
     }
 
-    if(userId !== user.userId){
+    if (userId !== user.userId) {
       throw new Error("유저 아이디가 일치하지 않습니다");
     }
 
-    if(user.emailStatus !== "completion"){
+    if (user.emailStatus !== "completion") {
       throw new Error("활성화 되지 않은 유저입니다.");
     }
 
     const passwordMatch = await bcrypt.compare(password, user.password);
 
     if (!passwordMatch) {
-        throw new Error("현재 비밀번호가 일치하지 않습니다.");
+      throw new Error("현재 비밀번호가 일치하지 않습니다.");
     }
-    
+
     const userdelete = await this.usersRepository.userdelete(userId);
 
     return userdelete;
-  }
-  
+  };
+
   aduserdelete = async (aduserId, adPassword) => {
     const aduser = await this.usersRepository.aduserById(aduserId);
-
-    if(aduserId !== aduser.aduserId){
+    if (aduserId !== aduser.aduserId) {
       throw new Error("어드민 아이디가 일치하지 않습니다");
     }
-
-    if(!aduser) {
+    if (!aduser) {
       throw new Error("어드민이 존재하지 않습니다");
     }
-
-    if(aduser.emailStatus !== "completion"){
+    if (aduser.adEmailStatus !== "completion") {
       throw new Error("활성화 되지 않은 유저입니다.");
     }
-
     const passwordMatch = await bcrypt.compare(adPassword, aduser.adPassword);
-
     if (!passwordMatch) {
-        throw new Error("현재 비밀번호가 일치하지 않습니다.");
+      throw new Error("현재 비밀번호가 일치하지 않습니다.");
     }
-
     const aduserdelete = await this.usersRepository.aduserdelete(aduserId);
-
-
     return aduserdelete;
-  }
+  };
 
   useraccess = async (email, verifiCationToken) => {
     const user = await this.usersRepository.getUserByEmail(email);
@@ -293,11 +291,10 @@ export class UsersService {
     return update;
   };
 
-
   getUserEmail = async (email) => {
     const user = await this.usersRepository.getUserByEmail(email);
     return user;
-  }
+  };
 
   getadUserEmail = async (adEmail) => {
     const aduser = await this.usersRepository.getAdByEmails(adEmail);
@@ -305,9 +302,8 @@ export class UsersService {
     return aduser;
   };
 
- getUserPoint = async (userId) => {
-        const point = await this.pointsRepository.getUserPoint(userId);
-        return point;
-    };
+  getUserPoint = async (userId) => {
+    const point = await this.pointsRepository.getUserPoint(userId);
+    return point;
+  };
 }
-
